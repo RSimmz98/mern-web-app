fa4f551e522518736b84b71a9ce6e33d2969f865
Richson Simbabure <richsonsimbabure@gmail.com>
2022-06-16 16:44:53 -0400

    created the routes folder and the routes file

---
+++ server/routes/routines.js
@@ -0,0 +1,35 @@
+  const express = require('express')
+
+  const router = expres.Router()
+
+
+   //creating routes
+  
+  //GET all routines
+  router.get('/', (req,res) => {
+    res.json({mssg: 'get all routines'})
+  })
+
+  //GET a single routines
+  router.get('/:id', (req,res) =>{
+    res.json({mssg: 'Get a single routine'})
+  })
+  
+  //POST a new routine
+  router.post('/',(req,res) =>{
+    res.json({mssg: 'POST a new routine'})
+  })
+
+  //DELETE a routine
+   router.delete('/:id',(req,res) =>{
+    res.json({mssg: 'DELETE a routine'})
+  })
+   
+   //UPDATE a routine
+   router.patch('/:id',(req,res) =>{
+    res.json({mssg: 'UPDATE a new routine'})
+  })
+
+
+
+  module.exports = router
