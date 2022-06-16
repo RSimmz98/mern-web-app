2418f69a1b0eb00e10b153065c775619e4f812f8
Richson Simbabure <richsonsimbabure@gmail.com>
2022-06-16 17:57:38 -0400

    added the client folder 🛴

---
+++ client/comingsoon.js
@@ -0,0 +1,8 @@
+yes the the client is coming learn with me my journey 
+of becoming a full stack developer 😁.. will 
+be creating the files soon please bare with me...
+thank you to those that are supporting me and please dont stop 
+.. learning is a process and it never fun so be patient
+dont give up it takes time to learn and I know it sucks sometimes but you \
+need to keep on going and you should never stop no matter the odds...
+lets keep going ... oops im not a motivational speaker but you will learn one or <two></two>
