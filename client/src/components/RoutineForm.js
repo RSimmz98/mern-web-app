3d94424f646be273aad93ae4328fc036a83c0ac4
Richson Simbabure <richsonsimbabure@gmail.com>
2022-07-01 20:19:49 +0200

    error handling 🏆

--- client/src/components/RoutineForm.js
+++ client/src/components/RoutineForm.js
@@ -7,7 +7,7 @@ import {useRoutinesContext} from '../hooks/useRoutinesContext'
     const [load, setLoad] = useState('');
      const [reps, setReps] = useState('');
       const [error, setError] = useState(null)
-
+       const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
       e.preventDefault()
@@ -27,13 +27,14 @@ import {useRoutinesContext} from '../hooks/useRoutinesContext'
 
     if(!response.ok){
       setError(json.error)
+      setEmptyFields(json.emptyFields)
   }
     if(response.ok){
       setTitle('')
       setLoad('')
       setReps('')
       setError(null)
-    
+      setEmptyFields([])
       console.log('new rourine has been created', json)
         dispatch({type: 'CREATE_ROUTINE', payload: json})
     
@@ -47,18 +48,21 @@ import {useRoutinesContext} from '../hooks/useRoutinesContext'
         type="text" 
         onChange = {(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
         />
          <label>Load (in days):</label>
       <input 
         type="number" 
         onChange = {(e) => setLoad(e.target.value)}
         value={load}
+         className={emptyFields.includes('load') ? 'error' : ''}
         />
          <label>Reps:</label>
       <input 
         type="number" 
         onChange = {(e) => setReps(e.target.value)}
         value={reps}
+         className={emptyFields.includes('reps') ? 'error' : ''}
         />
 
         <button>Add Routine</button>
