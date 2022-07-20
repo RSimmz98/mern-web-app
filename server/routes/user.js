312527857dab57ad6d52c4361bcecc97a443eb9a
Richson Simbabure <richsonsimbabure@gmail.com>
2022-07-20 23:25:03 +0200

    added the user auth

---
+++ server/routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const require = express.Router()
+
+//login Router
+ router.post('./login', () => {})
+
+//signup Router
+ router.post('./signup', () => {})
+
+module.exports = router
