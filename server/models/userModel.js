6f2e23b99bcee89bcc3f5eb04fa3b26dd6c43aaa
Richson Simbabure <richsonsimbabure@gmail.com>
2022-07-21 17:05:18 +0200

    added  validation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-
+const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema ({
@@ -17,6 +17,21 @@ const userSchema = new Schema ({
 
 //static signup method 
  userSchema.statics.signup = async function (email, password) {
+  //validation
+  if(!email || !password){
+    throw Error('All fields must be filled😒')
+  }
+
+  if(!validator.isEmail(email)) {
+    throw Error('Email is not valid😢')
+  }
+
+  if(!validator.isStrongPassword(password)){
+        throw Error('Password not strong💪🏿 enough')
+  }
+
+
+
   const exist = await this.findOne({email})
   
   if(exist) {
