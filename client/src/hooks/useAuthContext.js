ab4435d4114a011df9ae2b7307d23f655fcb01ab
Richson Simbabure <richsonsimbabure@gmail.com>
2022-07-27 12:34:38 +0200

    fixed authcontext

--- client/src/hooks/useAuthContext.js
+++ client/src/hooks/useAuthContext.js
@@ -2,7 +2,7 @@
  import {AuthContext} from '../context/AuthContext'
  import { useContext } from 'react'
 
-  export const useRoutinesContext = () => {
+  export const useAuthContext = () => {
     const context = useContext(AuthContext)
     
     if(!context) {
